75224e7e4445c2e41522d653a65feeeb1164da66
Robert Davis <robert@quarx.co>
2017-05-17 10:40:09 -0400

    Adding Mutliple Butotons

--- Assets/HoverCastMultiRadio.cs
+++ Assets/HoverCastMultiRadio.cs
@@ -6,29 +6,46 @@ using Hover.Core.Items;
 using Hover.Core.Items.Types;
 using Hover.Core.Items.Managers;
 using Hover.Core.Renderers.Shapes.Arc;
+using System;
 using System.IO;
+
 
 
 
 public class HoverCastMultiRadio : MonoBehaviour {
 
+	GameObject[] radioButtons;
+	string[] nameButtons;
+
 	// Use this for initialization
 	void Start () {
 
-		GameObject radioButton = new GameObject("Project1");
-		radioButton.AddComponent<HoverItemDataRadio> ().Label = "Project1";
-		HoverItemBuilder builder = radioButton.AddComponent<HoverItemBuilder> ();
-		builder.PerformBuild();
-		radioButton.AddComponent<HoverShapeArc> ();
-		radioButton.transform.parent = gameObject.transform;
-		radioButton.transform.localPosition = Vector3.zero;
+
+		string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Google Drive/Projects/";
+
+		string[] files = Directory.GetDirectories(@filepath);
 
 
-		string[] files = Directory.GetDirectories(@"/Users/robert/GitHub");
+
+		nameButtons = new string[files.Length];
+		radioButtons = new GameObject[files.Length];
 
 		for (int i = 0; i < files.Length; i++) {
 
-			Debug.Log (files.Length);
+			nameButtons[i] = files [i].Replace (filepath,"");
+			radioButtons[i] = new GameObject (nameButtons[i]);
+			radioButtons[i].AddComponent<HoverItemDataRadio> ().Label = nameButtons[i];
+			HoverItemBuilder builder = radioButtons[i].AddComponent<HoverItemBuilder> ();
+			builder.PerformBuild ();
+			radioButtons[i].AddComponent<HoverShapeArc> ();
+			radioButtons[i].transform.parent = gameObject.transform;
+			radioButtons[i].transform.localPosition = Vector3.zero;
+
+		}
+
+		for (int i = 0; i < files.Length; i++) {
+
+			Debug.Log (files[i]);
 		}
 	}
 	
